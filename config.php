df98be11097656af9fddcc8de20188b6cf1bed2c
kelvin <hope_kelvin@163.com>
2015-10-15 23:55:16 +0800

    2015-10-15

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php 
+define("DATABASE_HOST", "localhost");
+define("DATABASE_USERNAME", "username");
+define("DATABASE_PASSWORD", "password");
+define("DATABASE_NAME", "publications")
+ ?>
---
+++ registaccount.php
@@ -0,0 +1,8 @@
+<?php 
+$username=$_REQUEST['username'];
+$password=$_REQUEST['password'];
+$rePassword=$_REQUEST['repassword'];
+
+
+$
+ ?>
